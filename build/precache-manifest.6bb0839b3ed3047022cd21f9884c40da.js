0819a580fc0d95edab7a2833abc177fe8e4d9dbe
Matan Greenvlad <mgk2798@gmail.com>
2020-08-31 16:41:34 +0300

    build

---
+++ build/precache-manifest.6bb0839b3ed3047022cd21f9884c40da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a65e155786d2d1ce6f0bb3adb853d4e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6709a5515cc13fda95ae",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "770871f2a8925ffa48a0",
+    "url": "/static/js/2.586a0fc6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.586a0fc6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6709a5515cc13fda95ae",
+    "url": "/static/js/main.cb885fdb.chunk.js"
+  },
+  {
+    "revision": "aa03e00c16e389062aec",
+    "url": "/static/js/runtime-main.328dad9e.js"
+  }
+]);
