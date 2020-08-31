1f3d0bc6da5ecf5b3b98e798df58df09f3596ab9
Matan Greenvlad <mgk2798@gmail.com>
2020-08-31 11:35:48 +0300

    create front

---
+++ index.js
@@ -0,0 +1,93 @@
+const express = require('express')
+const app = express();
+const morgan = require('morgan')
+const cors = require('cors')
+
+app.use(express.json());
+app.use(morgan('dev'))
+app.use(cors())
+
+let persons = [
+    {
+        name: "Ilay Baruch",
+        number: "054-5363678",
+        id: 1
+      },
+      {
+        name: "Stav Zamir",
+        number: "054-4747207",
+        id: 2
+      },
+      {
+        name: "Omer Nir",
+        number: "054-6256258",
+        id: 3
+      },
+      {
+        name: "Ido Nochi",
+        number: "052-8328328",
+        id: 4
+      }
+]
+
+app.get('/', (req, res) => {
+  res.send(`<h1>Hello</h1>`)
+})
+
+app.get('/api/persons', (req, res) => {
+  res.json(persons)
+})
+
+app.get('/info', (req, res) => {
+    const personsAmount = persons.length;
+    res.send(
+        `<div>PhoneBook has info for ${personsAmount} persons</div>
+         <div>${new Date}</div>`)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const person = persons.find(person => person.id === id)
+    if (person) {
+        res.json(person)
+    } else {
+        res.status(404).end()
+    }
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    persons = persons.filter(person => person.id !== id)
+
+    res.status(204).end()
+})
+
+app.post('/api/persons', (req, res) => {
+    const body = req.body
+
+    if (!body.name || !body.number) {
+      return res.status(400).json({ 
+        error: 'name or number missing' 
+      })
+    } 
+    else if (persons.some(person => person.name === body.name)) {
+      return res.status(400).json({ 
+        error: 'name must be unique'
+      })
+    }
+
+    const newPerson = {
+        name: body.name,
+        number: body.number,
+        id: persons[persons.length - 1].id + 1
+    }
+
+    persons.push(newPerson);
+    res.json(persons);
+})
+
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
---
+++ phone-book/src/App.js
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+const baseUrl = 'http://localhost:3001/api/persons'
+
+function App() {
+  const [book, setBook] = useState([]);
+  const [name, setName] = useState();
+  const [number, setNumber] = useState();
+
+  const fetch = async () => {
+    const { data } = await axios.get(baseUrl)
+    setBook(data);
+  }
+
+  useEffect(() => {
+    fetch();
+  }, [])
+
+  const handleDelete = (e) => {
+    axios.delete(`${baseUrl}${e.target.id}`)
+    fetch();
+  }
+
+  const handleSubmit = async () => {
+    await axios.post(baseUrl, {
+      name, 
+      number
+    })
+    fetch();
+  }
+
+  return (
+    <div className="App">
+      <h1>Phone Book</h1>
+      <ul>
+        {book.map(item => 
+          <li>{item.name} {item.number} <button id={item.id} onClick={handleDelete}>delete</button></li>
+        )}
+      </ul>
+
+      <form onSubmit={handleSubmit}>
+        <input onChange={(e) => setName(e.target.value)} type='text' placeholder='name'/>
+        <input onChange={(e) => setNumber(e.target.value)} type='text' placeholder='number'/>
+        <button type='submit'>Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
