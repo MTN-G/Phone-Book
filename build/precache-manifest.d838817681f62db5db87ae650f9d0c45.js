71089232b8f9a3a7bb74a879b92ecbc6aced370d
Matan Greenvlad <mgk2798@gmail.com>
2020-08-31 12:09:37 +0300

    fix for heroku

---
+++ build/precache-manifest.d838817681f62db5db87ae650f9d0c45.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d02b5a044936cf27fe05894a88551c6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3cd7c04822bc6991104e",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "770871f2a8925ffa48a0",
+    "url": "/static/js/2.586a0fc6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.586a0fc6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3cd7c04822bc6991104e",
+    "url": "/static/js/main.c0e200b1.chunk.js"
+  },
+  {
+    "revision": "aa03e00c16e389062aec",
+    "url": "/static/js/runtime-main.328dad9e.js"
+  }
+]);
